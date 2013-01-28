8a7315de4fcd6aee2ce3a1e543863a53db5402a8
Jeremie Dimino <jdimino@janestreet.com>
2013-01-28 15:02:35 -0500

    108.09.00

--- lib_test/log_test.ml
+++ lib_test/log_test.ml
@@ -122,7 +122,7 @@ let tests = [
   "Log_test.write_and_read (bin-prot)", write_and_read `Bin_prot;
   "Log_test.speed_regression (sexp)", speed_test `Sexp (sec 8.);
   "Log_test.speed_regression (text)", speed_test `Text (sec 8.);
-  "Log_test.speed_regression (bin-prot)", speed_test `Bin_prot (sec 2.);
+  "Log_test.speed_regression (bin-prot)", speed_test `Bin_prot (sec 5.);
   "Log_test.rotation", rotation_test;
 ]
 
