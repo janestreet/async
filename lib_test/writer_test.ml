605f5e9b79bb9fedb6a6f934808314799e8e2335
Jeremie Dimino <jdimino@janestreet.com>
2013-01-28 15:09:26 -0500

    109.06.00

--- lib_test/writer_test.ml
+++ lib_test/writer_test.ml
@@ -68,6 +68,71 @@ let append max () =
     Unix.unlink file
 ;;
 
+let write_lots writer =
+  let ss = String.make 4096 's' in
+  for _i = 1 to 64 do
+    Writer.write writer ss
+  done
+
+let buffer_age_limit () =
+  Unix.pipe (Info.of_string "buffer_age_limit")
+  >>= function (`Reader reader_fd, `Writer writer_fd) ->
+    let writer_to_close = ref None in
+    try_with (fun () ->
+      let writer =
+        Writer.create writer_fd ~buffer_age_limit:(`At_most (sec 2.))
+      in
+      writer_to_close := Some writer;
+      write_lots writer;
+      after (sec 5.)
+      >>| fun () ->
+        `Did_not_raise_error)
+  >>= function
+    | Ok `Did_not_raise_error -> failwith "buffer-age check did not fire"
+    | Error _ ->
+      (* buffer-age check fired correctly *)
+      Writer.close ~force_close:(return ()) (Option.value_exn !writer_to_close)
+  >>= fun () ->
+    Unix.Fd.close reader_fd
+;;
+
+let reduce_buffer_age_limit () =
+  Unix.pipe (Info.of_string "buffer_age_limit")
+  >>= function (`Reader reader_fd, `Writer writer_fd) ->
+    let writer_to_close = ref None in
+    try_with (fun () ->
+      let writer =
+        Writer.create writer_fd ~buffer_age_limit:(`At_most (sec 180.))
+      in
+      writer_to_close := Some writer;
+      Writer.set_buffer_age_limit writer (`At_most (sec 1.));
+      write_lots writer;
+      after (sec 2.)
+      >>| fun () ->
+      `Did_not_raise_error)
+  >>= function
+  | Ok `Did_not_raise_error -> failwith "buffer-age check did not fire"
+  | Error _ ->
+    (* buffer-age check fired correctly *)
+    Writer.close ~force_close:(return ()) (Option.value_exn !writer_to_close)
+    >>= fun () ->
+    Unix.Fd.close reader_fd;
+;;
+
+let increase_buffer_age_limit () =
+  Unix.pipe (Info.of_string "buffer_age_limit")
+  >>= function (`Reader reader_fd, `Writer writer_fd) ->
+    let writer =
+      Writer.create writer_fd ~buffer_age_limit:(`At_most (sec 2.))
+    in
+    Writer.set_buffer_age_limit writer (`At_most (sec 180.));
+    write_lots writer;
+    after (sec 5.)
+  >>= fun () ->
+    Writer.close ~force_close:(return ()) writer
+  >>= fun () ->
+    Unix.Fd.close reader_fd
+
 let flush_on_close () =
   let module Debug = Async_core.Debug in
   let file = "flush_on_close.txt" in
@@ -114,12 +179,15 @@ let stdout () =
   return ()
 
 let tests = [
-  "Writer_test.write", write;
   "Writer_test.10 writers", multiple_writers 10;
   "Writer_test.100 writers", multiple_writers 100;
   "Writer_test.500 writers", multiple_writers 500;
   "Writer_test.append", append 1000;
+  "Writer_test.buffer_age_limit", buffer_age_limit;
   "Writer_test.flush on close", flush_on_close;
-  "Writer_test.stdout", stdout;
+  "Writer_test.increase_buffer_age_limit", increase_buffer_age_limit;
+  "Writer_test.reduce_buffer_age_limit", reduce_buffer_age_limit;
   "Writer_test.schedule_with_nonzero_pos", schedule_non_zero_pos;
+  "Writer_test.stdout", stdout;
+  "Writer_test.write", write;
 ]
