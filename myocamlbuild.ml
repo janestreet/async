2568dc941ab9a38de517aa46ea701cc6ae1bcac3
Jeremie Dimino <jdimino@janestreet.com>
2013-01-28 15:02:22 -0500

    108.00.02

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 9d34256701bb1a7c929832cb61ff17d4) *)
+(* DO NOT EDIT (digest: ac434c50282efddb2d4e8b6af73e50ac) *)
 module OASISGettext = struct
-# 21 "/tmp/oasis-0.3.0~rc5/src/oasis/OASISGettext.ml"
+(* # 21 "/tmp/oasis-0.3.0~rc5/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-# 21 "/tmp/oasis-0.3.0~rc5/src/oasis/OASISExpr.ml"
+(* # 21 "/tmp/oasis-0.3.0~rc5/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-# 21 "/tmp/oasis-0.3.0~rc5/src/base/BaseEnvLight.ml"
+(* # 21 "/tmp/oasis-0.3.0~rc5/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-# 21 "/tmp/oasis-0.3.0~rc5/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
+(* # 21 "/tmp/oasis-0.3.0~rc5/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-# 21 "/tmp/oasis-0.3.0~rc5/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+(* # 21 "/tmp/oasis-0.3.0~rc5/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-# 56 "/tmp/oasis-0.3.0~rc5/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+(* # 56 "/tmp/oasis-0.3.0~rc5/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
@@ -630,7 +630,10 @@ let setup_standard_build_flags () =
       List.concat (List.map f flags)
     in
     flag ["compile"; "c"] (S cflags);
-    flag ["compile"; "ocaml"] (S [A "-w"; A "@Aemr-28"; A "-strict-sequence" ])
+
+    (* enable warnings; make sure the '@' character isn't in the beginning;
+       ms-dos interprets that character specially *)
+    flag ["compile"; "ocaml"] (S [A "-w"; A "Aemr-28"; A "-strict-sequence" ])
 ;;
 
 let dispatch = function
