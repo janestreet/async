0c42b939c7853285ea03cdf172898145662e6876
Jeremie Dimino <jdimino@janestreet.com>
2013-02-11 13:22:33 -0500

    109.09.00
    - Switched `Async.Std`'s toplevel bindings for `Deferred.Or_error`'s `bind` and `map` to use
      `Deferred.Result`.
      This allows them to be used with any `'error` type, rather than just `Error.t`.

--- lib_test/thread_safe_test.ml
+++ lib_test/thread_safe_test.ml
@@ -1,19 +1,32 @@
 open Core.Std
 open Async.Std
 
-let tests : (unit -> unit Deferred.t) list ref = ref []
+let tests : (string * (unit -> unit Deferred.t)) list ref = ref []
 
-let test f = tests := f :: !tests
+let test name f =
+  tests := (String.concat [ "Thread_safe: "; name ], f) :: !tests
+;;
 
 let () =
-  test (fun () ->
+  test "run_in_async_with_optional_cycle must be in thread" (fun () ->
+    begin try
+      ignore (Thread_safe.run_in_async_with_optional_cycle (fun () -> (`Run_a_cycle, ()))
+                : (unit, exn) Result.t);
+      assert false;
+    with _ -> ()
+    end;
+    Deferred.unit)
+;;
+
+let () =
+  test "not am_holding_async_lock in a thread" (fun () ->
     assert (Thread_safe.am_holding_async_lock ());
     In_thread.run (fun () ->
       assert (not (Thread_safe.am_holding_async_lock ()))))
 ;;
 
 let () =
-  test (fun () ->
+  test "run_in_async_exn am_holding_async_lock" (fun () ->
     assert
       (try Thread_safe.run_in_async_exn (fun () -> assert false); false
        with _ -> true);
@@ -24,7 +37,7 @@ let () =
 ;;
 
 let () =
-  test (fun () ->
+  test "run_in_async_exn" (fun () ->
     let message = "foo" in
     Monitor.try_with (fun () ->
       In_thread.run (fun () ->
@@ -39,7 +52,7 @@ let () =
 ;;
 
 let () =
-  test (fun () ->
+  test "deferred" (fun () ->
     In_thread.run (fun () ->
       let (d, put) = Thread_safe.deferred () in
       put 13;
@@ -52,7 +65,7 @@ let () =
 ;;
 
 let () =
-  test (fun () ->
+  test "deferred2" (fun () ->
     In_thread.run (fun () -> Thread_safe.deferred ())
     >>= fun (d, put) ->
     assert (try put 13; false with _ -> true);
@@ -61,31 +74,47 @@ let () =
     >>| fun i ->
     assert (i = 13);
   )
-;;
+
+let log_string = Async_core.Debug.log_string
 
 let () =
-  test (fun () ->
-    In_thread.run (fun () -> Thread_safe.pipe ())
-    >>= fun (pipe, put, close) ->
-    assert (try put 13; false with _ -> true);
+  test "Thread_safe_pipe" (fun () ->
+    let module P = Thread_safe_pipe in
+    let r, p = P.create () in
+    assert (try P.write p 13; false with _ -> true);
     let throttle = Throttle.create ~continue_on_error:false ~max_concurrent_jobs:1 in
     let run_in_thread f =
       don't_wait_for (Throttle.enqueue throttle (fun () -> In_thread.run f))
     in
     let num_elts = 100 in
     for i = 0 to num_elts - 1; do
-      run_in_thread (fun () -> put i);
+      run_in_thread (fun () -> P.write p i);
     done;
-    run_in_thread (fun () -> close ());
-    Pipe.to_list pipe
+    run_in_thread (fun () -> P.close p);
+    Pipe.to_list r
     >>| fun list ->
     assert (list = List.init num_elts ~f:Fn.id);
   )
 ;;
 
-let tests =
-  [ "Thread_safe_test",
-    (fun () -> Deferred.List.iter !tests ~f:(fun f -> f ()))
-  ]
+let () =
+  test "Thread_safe_pipe2" (fun () ->
+    In_thread.run (fun () -> Thread_safe_pipe.create ())
+    >>= fun (pipe_reader, pipe_writer) ->
+    assert (try Thread_safe_pipe.write pipe_writer 13; false with _ -> true);
+    let throttle = Throttle.create ~continue_on_error:false ~max_concurrent_jobs:1 in
+    let run_in_thread f =
+      don't_wait_for (Throttle.enqueue throttle (fun () -> In_thread.run f))
+    in
+    let num_elts = 100 in
+    for i = 0 to num_elts - 1; do
+      run_in_thread (fun () -> Thread_safe_pipe.write_without_pushback pipe_writer i);
+    done;
+    run_in_thread (fun () -> Thread_safe_pipe.close pipe_writer);
+    Pipe.to_list pipe_reader
+    >>| fun list ->
+    assert (list = List.init num_elts ~f:Fn.id);
+  )
 ;;
 
+let tests = !tests
